ce5b3478a594fdfd06df8b0384eec8b92f2bc23b
Shrideep_Tamboli <27.tshrideep@gmail.com>
2025-04-21 03:00:53 +0530

    Fix: Login with Github

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com',
+      'avatars.githubusercontent.com'
+    ],
   },
 }
 
